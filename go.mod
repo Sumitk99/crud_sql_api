0f21e6c93f1c5ebe3dfb12dcdc12531a776d5430
Sumitk99 <sumit2k19kumar@gmail.com>
2024-05-14 18:01:29 +0530

    first commit

--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module crud_api
 go 1.19
 
 require github.com/gorilla/mux v1.8.1 // indirect
+require (
+    github.com/go-sql-driver/mysql v1.5.0 // or whatever the latest version is
+)
+
--- main.go
+++ main.go
@@ -1,71 +1,105 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-type Message struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
+type Entry struct {
+	Date string `json:"date"`
+	Day  string `json:"day"`
+	Task string `json:"task"`
 }
 
-
-var db = make(map[string]string)
+var db *sql.DB
 
 func read_func(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("Reading Initiated")
 	res.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(res).Encode(db)
+	query := "SELECT * FROM entries"
+	result, err := db.Query(query)
+	defer result.Close()
+	var output []Entry
+	for result.Next() {
+		var a, b, c string
+		result.Scan(&a, &b, &c)
+		fmt.Printf("%s %s %s\n", a, b, c)
+		output = append(output, Entry{a, b, c})
+	}
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+	}
+	json.NewEncoder(res).Encode(output)
 }
 
 func create_func(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
-	var message Message
-	err := json.NewDecoder(req.Body).Decode(&message)
+	var new_entry Entry
+	err := json.NewDecoder(req.Body).Decode(&new_entry)
+	stmt, err := db.Prepare("INSERT INTO entries (date, day, task) VALUES (?, ?, ?)")
 	if err != nil {
-		http.Error(res, err.Error(), http.StatusBadRequest)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	db[message.Key] = message.Value
-	json.NewEncoder(res).Encode(db[message.Key])
+	defer stmt.Close()
+
+	_, err = stmt.Exec(new_entry.Date, new_entry.Day, new_entry.Task)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(res).Encode(new_entry)
 }
 
 func update_func(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
-	var message Message
-	err := json.NewDecoder(req.Body).Decode(&message)
+	var updated_entry Entry
+	err := json.NewDecoder(req.Body).Decode(&updated_entry)
+
+	stmt, err := db.Prepare("UPDATE entries SET task = ? WHERE date = ?")
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
 	}
-	db[message.Key] = message.Value
-	json.NewEncoder(res).Encode(message)
+	stmt.Exec(updated_entry.Task, updated_entry.Date)
+	json.NewEncoder(res).Encode(updated_entry)
 }
 
 func delete_func(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
-	var message Message
-	err := json.NewDecoder(req.Body).Decode(&message)
+	var del_entry Entry
+	err := json.NewDecoder(req.Body).Decode(&del_entry)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
 	}
-	db[message.Key] = message.Value
-	delete(db, message.Key)
-	json.NewEncoder(res).Encode(db)
+
+	stmt, err := db.Prepare("DELETE FROM entries WHERE date = ?")
+	stmt.Exec(del_entry.Date)
+	json.NewEncoder(res).Encode(del_entry)
 }
 
 func main() {
 	router := mux.NewRouter()
-	db["123"] = "456"
-	db["343"] = "333"
-	router.HandleFunc("/read", read_func).Methods("GET")
-	router.HandleFunc("/delete", delete_func).Methods("DELETE")
-	router.HandleFunc("/create", create_func).Methods("POST")
-	router.HandleFunc("/update", update_func).Methods("PUT")
+	var err error
+	defer db.Close()
+
+	db, err = sql.Open("mysql", "root:@tcp(0.0.0.0:3306)/dbnew")
+	err = db.Ping()
+	//_, err = db.Exec("CREATE TABLE entries(date VARCHAR(20) PRIMARY KEY , day VARCHAR(20), task VARCHAR(100) );")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	router.HandleFunc("/", read_func).Methods("GET")
+	router.HandleFunc("/", delete_func).Methods("DELETE")
+	router.HandleFunc("/", create_func).Methods("POST")
+	router.HandleFunc("/", update_func).Methods("PUT")
 
 	fmt.Println("Starting server on port 9000")
 	log.Fatal(http.ListenAndServe(":9000", router))
